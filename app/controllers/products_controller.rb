b15db79591c9b28d31898c05727647711b0edb5e
Demario Marbray <toosexybuff500@gmail.com>
2017-03-14 18:09:22 -0700

    added rows and getting add create edit button to work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,73 @@
 class ProductsController < ApplicationController
+
+
+	def index
+		
+		@products = Product.all	
+		render 'index.html.erb'
+
+	end
+
+	def show
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id )
+	end
+
+	def shirt
+	end
+	
+
+	def new
+
+	end
+
+	def create
+		input_name = params[:product_name]
+		input_price = params[:price ]
+		input_description = params[:description]
+		input_image = params[:image]
+		@product = Product.create( name: input_name, price: input_price, description: input_description, image: input_image )
+		flash[:success] = "product was created successfully!"
+		redirect_to "/products"	
+	end
+		
+	def edit
+		product_id = params[:id]
+		@product = Product.find_by(id: product_id)
+
+		
+	end
+
+	def update
+
+		@product = Product.find_by(id: params[:id])
+		@product.name = params[:product_name]
+		@product.price = params[:price]
+		@product.description = params[:product_description]
+		@product.image = params[:image]
+		@product.save
+		
+		flash[:info] = "product updated successfully!"
+      	redirect_to "/products"
+	end
+	def destroy
+		@product = Product.find_by(id: params[:id])
+		@product.destroy		
+		flash[:danger] = "product deleted successfully!"
+		redirect_to "/products"
+		
+	end
+
+
+	def newshirt
+
+		input_name = params[:product_name]
+		input_price = params[:price ]
+		input_description = params[:description]
+		input_image = params[:image]
+		@product = Product.create( name: input_name, price: input_price, description: input_description, image: input_image )
+	end
+	
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  		get "/products" => "products#index"
+
+  		# new and create 
+  		get "/products/new" => "products#new"
+  		post "/products" => "products#create"
+  		# show
+  		get "/products/:id" => "products#show"
+  		#edit
+  		get "/products/:id/edit" => "products#edit"
+  		patch "/products/:id" => "products#update"
+  		#delete
+  		delete "/products/:id" => "products#destroy"
+
+
+
+
+
+  		get"/new_product_line" => "products#shirt"
+  		post "/new_product" => "products#newshirt"
+
+
+  		
+
 end
